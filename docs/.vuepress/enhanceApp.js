10a39d10153f553c26b73e5f5387eaadecc61040
董展 <dongzhan@dongzhandeMacBook-Pro.local>
2019-09-14 17:46:20 +0800

    feat(app):add sldebar-link init active

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,11 +1,20 @@
-import Vue from 'vue'
 
 
 export default ({
-  Vue,
-  options,
-  router,
-  route
+  Vue, // VuePress 正在使用的 Vue 构造函数
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData // 站点元数据
 }) => {
-  
+  router.afterEach((to, from, next) => {
+    // ...
+    // encodeURIComponent
+    setTimeout(() => {
+      let hashList = Array.prototype.map.call(document.querySelectorAll('.sidebar-link'),(item => item.hash));
+      let index = hashList.findIndex(item => item == to.hash);
+      document.querySelectorAll('.sidebar-link')[index].className = 'active' + ' sidebar-link'
+    }, 300);
+    
+    
+  })
 }
